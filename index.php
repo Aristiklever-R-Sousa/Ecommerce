e1826cfafb3ac73642382872d5d2a1ad7706aa60
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2020-07-31 18:36:35 -0300

    Criação da class PageAdmin e do tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 	use \Slim\Slim;
 	use \Hcode\Page;
+	use \Hcode\PageAdmin;
 
 	$app = new Slim();
 
@@ -12,6 +13,14 @@
 	$app->get('/', function() {
 	    
 	    $page = new Page();
+
+	    $page->setTpl("index");
+
+	});
+
+	$app->get('/admin', function() {
+	    
+	    $page = new PageAdmin();
 
 	    $page->setTpl("index");
 
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,17 @@
+<?php
+
+	namespace Hcode;
+
+	use Rain\Tpl;
+
+	class PageAdmin extends Page
+	{
+
+		public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+		{
+			parent::__construct($opts, $tpl_dir);
+		}
+
+	}
+
+?>
