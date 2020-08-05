4bc03c95d70bb467d724ab6820a51cb259bd0eb8
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2020-08-05 17:28:50 -0300

    Building of the users' CRUD

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -31,8 +31,7 @@
 				$user = new User();
 			
 				$user->setData($data);
-				// var_dump($user);
-				// exit;
+			
 				$_SESSION[User::SESSION] = $user->getData();
 
 			}else{
@@ -53,13 +52,105 @@
 				(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
 			) {
 				header("Location: /admin/login");
-				exit;
+				exit("Tente novamente");
 			}
 		}
 
 		public static function logout()
 		{
 			$_SESSION[User::SESSION] = NULL;
+		}
+
+		public static function listAll() 
+		{
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_users a
+									INNER JOIN
+									tb_persons b
+									USING(idperson)
+									ORDER BY b.desperson"
+								);
+
+		}
+
+		public function save()
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select(
+				"CALL sp_users_save(
+					:desperson, :deslogin,
+					:despassword, :desemail,
+					:nrphone, :inadmin
+				)",
+				array(
+					":desperson" => $this->getdesperson(),
+					":deslogin" => $this->getdeslogin(),
+					":despassword" => $this->getdespassword(),
+					":desemail" => $this->getdesemail(),
+					":nrphone" => $this->getnrphone(),
+					":inadmin" => $this->getinadmin()
+				)
+			);
+
+			$this->setData($results[0]);
+		}
+
+		public function get($iduser)
+		{
+			
+			$sql = new Sql();
+
+			$results = $sql->select(
+				"SELECT * FROM tb_users a
+				INNER JOIN tb_persons b
+				USING(idperson)
+				WHERE a.iduser = :iduser", array(
+					":iduser" => $iduser
+				)
+			);
+
+			$this->setData($results[0]);
+
+		}
+
+		public function update()
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select(
+				"CALL sp_usersupdate_save(
+					:iduser,
+					:desperson, :deslogin,
+					:despassword, :desemail,
+					:nrphone, :inadmin
+				)",
+				array(
+					":iduser" => $this->getiduser(),
+					":desperson" => $this->getdesperson(),
+					":deslogin" => $this->getdeslogin(),
+					":despassword" => $this->getdespassword(),
+					":desemail" => $this->getdesemail(),
+					":nrphone" => $this->getnrphone(),
+					":inadmin" => $this->getinadmin()
+				)
+			);
+
+			$this->setData($results[0]);
+		}
+
+		public function delete()
+		{
+
+			$sql = new Sql();
+
+			$sql->query("CALL sp_users_delete(:iduser)", array(
+				":iduser" => $this->getiduser()
+			));
+
 		}
 
 	}
