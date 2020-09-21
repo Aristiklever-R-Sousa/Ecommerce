af6e90b20a72e6ef2db5072dc2363b79058fa4b9
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2020-09-21 10:58:29 -0300

    Pagination

--- routes/home.php
+++ routes/home.php
@@ -18,15 +18,29 @@
 
 	$app->get("/categories/:idcategory", function($idcategory) {
 
+		$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
 		$category = new Category();
 
 		$category->get((int)$idcategory);
+
+		$pagination = $category->getProductsPage($page);
+
+		$pages = [];
+
+		for ($i = 1; $i <= $pagination['pages']; $i++) { 
+			array_push($pages, [
+				'link' => '/categories/' .$category->getidcategory(). '?page='. $i,
+				'page' => $i
+			]);
+		}
 
 		$page = new Page();
 
 	    $page->setTpl("category", [
 	    	'category' => $category->getData(),
-	    	'products' => Product::checkList($category->getProducts())
+	    	'products' => $pagination['data'],
+	    	'pages' => $pages
 	    ]);
 
 	});
