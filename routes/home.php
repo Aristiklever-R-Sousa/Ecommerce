31039cca45ccd3fd7d6e9ce55ec44f0538304f84
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2020-09-30 11:38:30 -0300

    Construction of the User password change.

--- routes/home.php
+++ routes/home.php
@@ -617,4 +617,75 @@
 
 	});
 
+	$app->get("/profile/change-password", function() {
+
+		User::verifyLogin(false);
+
+		$page = new Page();
+
+		$page->setTpl("profile-change-password", [
+			'changePassError' => User::getError(),
+			'changePassSuccess' => USer::getSuccess()
+		]);
+
+	});
+
+	$app->post("/profile/change-password", function() {
+
+		User::verifyLogin(false);
+
+		if(!isset($_POST['current_pass']) || $_POST['current_pass'] == '') {
+
+			User::setError("Fill in your current password.");
+			header('Location: /profile/change-password');
+			exit;
+
+		}
+
+
+		if(!isset($_POST['new_pass']) || $_POST['new_pass'] == '') {
+
+			User::setError("Fill in your new password.");
+			header('Location: /profile/change-password');
+			exit;
+
+		}
+
+		if(!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] == '') {
+
+			User::setError("Fill in the confirmation of the new password.");
+			header('Location: /profile/change-password');
+			exit;
+
+		}
+
+		if($_POST['new_pass'] != $_POST['new_pass_confirm']) {
+
+			User::setError("Password confirmation and new password don't match.");
+			header('Location: /profile/change-password');
+			exit;
+
+		}
+
+		$user = User::getFromSession();
+
+		if(!password_verify($_POST['current_pass'], $user->getdespassword())) {
+
+			User::setError("Your current password is incorrect. Try again.");
+			header('Location: /profile/change-password');
+			exit;
+
+		}
+
+		$user->setdespassord($_POST['new_pass']);
+
+		$user->update();
+
+		User::setSuccess("Password changed successfully!");
+
+		header('Location: /profile/change-password');
+		exit;
+
+	});
+
 ?>
