3374796e36e889f629ec4322bdb9bda3f53c9329
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2020-10-02 10:40:09 -0300

    Creation of User pagination and search on the admin-side

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -473,6 +473,68 @@
 
 		}
 
+		public static function getPage($page = 1, $itemsPerPage = 10)
+		{
+
+			$start = ($page - 1) * $itemsPerPage ;
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT SQL_CALC_FOUND_ROWS *
+				FROM tb_users u
+				INNER JOIN tb_persons p
+					ON u.idperson = p.idperson
+				ORDER BY p.desperson
+				LIMIT $start, $itemsPerPage;
+			");
+
+			$resultTotal = $sql->select("
+				SELECT FOUND_ROWS() AS nrtotal;
+			");
+
+			return [
+				'data' => $results,
+				'totalRows' => $resultTotal[0]['nrtotal'],
+				'pages' => ceil($resultTotal[0]['nrtotal'] / $itemsPerPage)
+			];
+
+		}
+
+		public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+		{
+
+			$start = ($page - 1) * $itemsPerPage ;
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT SQL_CALC_FOUND_ROWS *
+				FROM tb_users u
+				INNER JOIN tb_persons p
+					ON u.idperson = p.idperson
+				WHERE p.desperson LIKE :search
+					OR p.desemail = :search
+					OR u.deslogin LIKE :search
+				ORDER BY p.desperson
+				LIMIT $start, $itemsPerPage;
+			", [
+				':search' => '%'.$search.'%'
+			]);
+
+			$resultTotal = $sql->select("
+				SELECT FOUND_ROWS() AS nrtotal;
+			");
+
+			return [
+				'data' => $results,
+				'totalRows' => $resultTotal[0]['nrtotal'],
+				'pages' => ceil($resultTotal[0]['nrtotal'] / $itemsPerPage)
+			];
+
+		}
+
+
 	}
 
 ?>
